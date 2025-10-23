3cd4d5ecf81df66e181aab20ed65b5d7ede7686f
V1ctor2911 <omar29112004@gmail.com>
2025-10-23 18:46:36 +0200

    Primer cambio

--- hola.c
+++ hola.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    int arg = argc - 1;
     int x;
+    printf("Se han recibido %d argumentos.\n", arg);
+    if (argc == 1){
+        printf("No se ha recibido ningun argumento\n");
+    } else if (argc > 3) {
+        printf("No se puede usar esta funcion con tantos argumentos, maximo 2, deben ser enteros.\nError fatal.");
+        return 1;
+    }
     printf("%d \n", argc);
     printf("Hola, aaluc!\n");
     scanf("%d", &x);

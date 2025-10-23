c0ac7c36fb2812f7674b47925897083050841026
V1ctor2911 <omar29112004@gmail.com>
2025-10-23 15:37:52 +0200

    Libreria

--- hola.c
+++ hola.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(int argc) {
+int main(int argc, char *argv[]) {
+    int x;
     printf("%d \n", argc);
     printf("Hola, aaluc!\n");
+    scanf("%d", &x);
+    printf("El numero es: %d\n", x);
     return 0;
 }

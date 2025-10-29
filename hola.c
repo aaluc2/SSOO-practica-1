51a0f570cb4d87007cbfa9ee9506af10f6404ff5
V1ctor2911 <omar29112004@gmail.com>
2025-10-29 09:21:41 +0100

    ola

--- hola.c
+++ hola.c
@@ -2,19 +2,17 @@
 
 int main(int argc, char *argv[]) {
     int arg = argc - 1;
-    char buffer[100];
+    char aalic[100];
     int i;
     int count = 3;
     printf("Escribe algo: ");
 
     for (i = 0; i < count; i++)
     {
-        fgets(buffer, 100, stdin);
-        printf(buffer);
+        fgets(aalic, 100, stdin);
+        printf(aalic);
     }
- 
-    return 0;
-
+    
     printf("Se han recibido %d argumentos.\n", arg);
     if (argc == 1){
         printf("No se ha recibido ningun argumento\n");
@@ -22,6 +20,7 @@ int main(int argc, char *argv[]) {
         printf("No se puede usar esta funcion con tantos argumentos, maximo 2, deben ser enteros.\nError fatal.");
         return 1;
     }
+    printf("Se ha recibido %s, como argumento",argv[0]);
     printf("%d \n", argc);
     printf("Hola, aaluc!\n");
 

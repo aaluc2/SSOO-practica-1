ca3051f5e40576ae3e00af5a18b54df0e02a2103
V1ctor2911 <omar29112004@gmail.com>
2025-11-02 16:37:10 +0100

    Mini cambio

--- test.c
+++ test.c
@@ -4,12 +4,18 @@
 #include <string.h>
 int main(int argc, char *argv[]){
    int N ; 
-    if (strcmp(argv[1], "head")==0) {
+   char error[]="Hola";
+   if (argc==1) {
+    printf(error);
+    exit(1);
+   }
+   
+    if (strcmp(argv[1], "-head")==0) {
         N = 10;
-        printf("Ejecutando head con N = %d\n", N);
         if (argc == 3) {
         N = atoi(argv[2]); 
         }
+        printf("Ejecutando head con N = %d\n", N);
         head(N);
     }else{
         printf("Comando no reconocido.\n");

b75d3caaf9b75d3eed44b0e28fffb414b9c545ba
V1ctor2911 <omar29112004@gmail.com>
2025-11-06 14:50:45 +0100

    q

--- test.c
+++ test.c
@@ -4,22 +4,26 @@
 #include <string.h>
 int main(int argc, char *argv[]){
    int N ; 
-   char error[]="Error: uso incorrecto.\nFormato valido: test -<funcion> [numero_de_lineas].\nSi no se especifica numero_de_lineas, se usaran 10 por defecto.\n";
    if (argc==1) {
-    printf(error);
+    printf("Error por uso incorrecto.\nFormato válido para %s: test -<función> [N lineas].\nSólo se podrán usar 3 funciones: 'head', 'tail' o 'longlines' e irán precedidas por un '-'.\nSi no se especifica número de lineas, se usarán 10 por defecto.\nSe lee de la entrada estándar(redireccionable).\n",argv[0]);
     exit(1);
    }
    
-    if (strcmp(argv[1], "-head")==0) {
+    if (strcmp(argv[1], "-head") == 0) {
         N = 10;
         if (argc == 3) {
         N = atoi(argv[2]); 
         }
-        printf("Ejecutando head con N = %d\n", N);
         head(N);
-    }else{
-        printf("Comando no reconocido.\n");
-    }  
+    }else if (strcmp(argv[1], "-tail") == 0){
+        N = 10;
+        if (argc == 3) {
+            N = atoi(argv[2]);
+        }
+        tail(N);
+    }  else {
+        printf("Error por uso incorrecto.\nFormato válido para %s: test -<función> [N lineas].\nSólo se podrán usar 3 funciones: 'head', 'tail' o 'longlines' e irán precedidas por un '-'.\nSi no se especifica número de lineas, se usarán 10 por defecto.\nSe lee de la entrada estándar(redireccionable).\n",argv[0]);
+    }
     
 
     return 0;

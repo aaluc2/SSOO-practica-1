7ee84f306d321744b6693fce106e21eb6bfb9d46
adamkas <adlandsnte42@gmail.com>
2025-11-06 21:28:33 +0100

    tail implemented

--- libreria.c
+++ libreria.c
@@ -3,9 +3,8 @@
 
 
 int head(int N) {
-    //No hace nada xd
     if (N <= 0) {
-        printf("El numero de lineas debe ser mayor que 0.\n");
+        printf("head: invalid trailing option -- %d.\n", N);
         return 0; 
     }
 
@@ -22,45 +21,39 @@ int head(int N) {
 }
 
 int tail(int N) {
-    int inicio;
-    int count;
-    if (N <= 0) {
-        printf("El numero de lineas debe ser mayor que 0.\n");
+ FILE *fp = fopen("ftail.txt", "w");
+    if (!fp) {
+        printf("Error al abrir el fichero para escritura");
         return 0;
     }
 
-    if (N > 1024) {
-        printf("El numero maximo de lineas soportado es 1024.\n");
-        return 0;
-    }
-
-    char lineas[1024][1024];
+    char buffer[1024];
+    int contador = 0;
     int total = 0;
+    int count = 0;
 
-    while (fgets(lineas[total % N], sizeof(lineas[0]), stdin) != NULL) {
-        total++;
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        fputs(buffer, fp);
+        contador++;
     }
+    fclose(fp);
 
+    fp = fopen("ftail.txt", "r");
+    printf("Total de lineas leidas: %d\n", contador);
     //Si total es 0 no tenemos nada que escribir y por eso se devuelve 0 directamente.
-    if (total == 0) {
-        return 0;
-    }
-    
-    if (total > N) {
-        inicio = total % N;
-    } else {
-        inicio = 0;
-    }
-    
-    if (total < N) {
-        count = total;
-    } else {
-        count = N;
-    }
+    total = contador - N;
+    printf("Lineas a omitir: %d\n", total);
 
-    for (int i = 0; i < count; i++) {
-        int idx = (inicio + i) % N;
-        fputs(lineas[idx], stdout);
+    while (count < contador && fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (count < total)
+        {
+            count++;  
+        }else{
+            fputs(buffer, stdout);
+            count++;
+        }
+        
+        
     }
 
     return 0;

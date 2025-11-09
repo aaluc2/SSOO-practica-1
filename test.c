2d30f3a96062de6e6d7b6253760a786553c709dc
V1ctor2911 <omar29112004@gmail.com>
2025-11-09 19:10:09 +0100

    .

--- test.c
+++ test.c
@@ -3,24 +3,28 @@
 #include "libreria.h"
 #include <string.h>
 int main(int argc, char *argv[]){
+    //Variables locales
    int N ; 
+   //Control de argumentos
    if (argc==1) {
     printf("Error por uso incorrecto.\nFormato válido para %s: test -<función> [N lineas].\nSólo se podrán usar 3 funciones: 'head', 'tail' o 'longlines' e irán precedidas por un '-'.\nSi no se especifica número de lineas, se usarán 10 por defecto.\nSe lee de la entrada estándar(redireccionable).\n",argv[0]);
     exit(1);
    }
-   
+   //Comprobación head
     if (strcmp(argv[1], "-head") == 0) {
         N = 10;
         if (argc == 3) {
         N = atoi(argv[2]); 
         }
         head(N);
+    //Comprobación tail
     }else if (strcmp(argv[1], "-tail") == 0){
         N = 10;
         if (argc == 3) {
             N = atoi(argv[2]);
         }
         tail(N);
+    //Comprobación longliness
     }  else if (strcmp(argv[1], "-longlines") == 0){
         N = 10;
         if (argc == 3){
@@ -28,11 +32,12 @@ int main(int argc, char *argv[]){
         }
         longlines(N);
     }
+    //Mensaje incorrecto
     else {
         printf("Error por uso incorrecto.\nFormato válido para %s: test -<función> [N lineas].\nSólo se podrán usar 3 funciones: 'head', 'tail' o 'longlines' e irán precedidas por un '-'.\nSi no se especifica número de lineas, se usarán 10 por defecto.\nSe lee de la entrada estándar(redireccionable).\n",argv[0]);
         exit(2);
     }
     
-
+    //Ejecución correcta, retorno 0
     return 0;
 }
